4bfc869a61a525c3c3cf20655c7c931e67774619
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-17 20:39:23 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
   else
-    #new_array = []
+    new_array = []
     #katz.each do |name|
     #new_array << "(i + 1). array[i] "
     ## puts "The line is currently : {new_array.join}"
