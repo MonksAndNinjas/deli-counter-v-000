332122559d19aba096a1c62824497c1ed8678c27
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-17 21:18:10 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,8 +6,8 @@ def line(katz_deli)
     new_array = []
     i = 0
     katz_deli.each do |name|
-      name_num = [i, ., name]join
-      puts
+      name_num = [i, ., name].join
+      puts name_num
       i += 1
     end
     #puts "The line is currently: " + new_array.join(" ")
