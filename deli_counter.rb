0b6a288e012a41df10f97a71bc27ebbdef7dee67
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-17 20:37:15 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,5 +2,9 @@
 def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
+  else
+    #new array << "(i + 1). array[i] "
+    ## puts "The line is currently : 
+    puts "The line is currently: i array[i]"
   end
 end
